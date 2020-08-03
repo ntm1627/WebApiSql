c4c6a67763e6ca693d17e3544e633f22781f65db
Naod Agere <ntm1627>
2020-08-03 16:06:31 -0700

    DI is added and connection is set

--- WebApiSql/Startup.cs
+++ WebApiSql/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebApiSql.Models;
 
 namespace WebApiSql
 {
@@ -25,6 +27,8 @@ namespace WebApiSql
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("InventoryDatabase");
+            services.AddDbContextPool<InventoryContext>(options => options.UseSqlServer(connection));
             services.AddControllers();
         }
 
